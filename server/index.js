8d0df5af890264653c51ef3cf0170045ef5e2491
sandeep7567 <sandeepthakur7567@gmail.com>
2023-09-18 16:01:45 +0530

    initial backend init

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:["*", "http://localhost:3000"],
+		origin:["*", "http://localhost:3000", "http://studynotion-backend-app.onrender.com"],
 		credentials:true,
 	})
 )
